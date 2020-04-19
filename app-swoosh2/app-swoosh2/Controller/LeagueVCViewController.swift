ccb0430862491c27c627d44197406f47da93a71b
Xanathos <xanathosd2x@gmail.com>
2020-04-18 19:16:01 -0500

    Programmatic segues

---
+++ app-swoosh2/app-swoosh2/Controller/LeagueVCViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LeagueVCViewController.swift
+//  app-swoosh2
+//
+//  Created by Christian Torrico on 4/12/20.
+//  Copyright © 2020 Christian Torrico. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ app-swoosh2/app-swoosh2/Controller/SkillVC.swift
@@ -0,0 +1,17 @@
+//
+//  SkillVC.swift
+//  app-swoosh2
+//
+//  Created by Christian Torrico on 4/12/20.
+//  Copyright © 2020 Christian Torrico. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
